a255ef8dc906ea2f91c34b5664bfe943a6b523dd
Vít Barták <vitabartak@gmail.com>
2023-02-01 21:19:27 +0100

    03-03 chapter start

--- haskell_html/hello.hs
+++ haskell_html/hello.hs
@@ -2,21 +2,20 @@ main :: IO()
 main = putStrLn myhtml
 
 myhtml :: String
-myhtml = makeHtml "Hello title" "Hello, world!"
+myhtml = 
+    makeHtml 
+        "Hello title" 
+        (h1_ "Hello, world!" <> p_ "world")
 
 makeHtml :: String -> String -> String
 makeHtml title content = html_ (head_ (title_ title) <> body_ content)
 
 -- <> is operator for string concatenation
 head_ :: String -> String
-head_ head =
-    "<head>" <> head <> "</head>"
+head_  = el "head"
 
 title_ :: String -> String
-title_ title = 
-    "<title>" <> title <> "</title>"
-
--- Partial application
+title_  = el "title"
 
 html_ :: String -> String
 html_ = el "html" 
@@ -24,6 +23,21 @@ html_ = el "html"
 body_ :: String -> String
 body_ = el "body" 
 
+h1_ :: String -> String
+h1_ = el "h1"
+
+p_ :: String -> String
+p_ = el "p"
+
+-- Partial application
 el :: String -> String -> String
 el tag content =
-  "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
+    "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
+
+
+-- Anonymous lambda function
+--three :: Integer
+--three = (\num1 -> \num2 -> num1 + num2) 1 2
+
+--three2 :: Integer
+--three2 = (\num1 num2 -> num1 + num2) 1 2
